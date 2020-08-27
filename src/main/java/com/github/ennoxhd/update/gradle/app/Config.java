5f263c7ff56f745c77be50a09ddff6b03f5ff87a
EnnoxHD <henrik.schirmacher@outlook.de>
2020-08-27 20:05:39 +0200

    more options and refactor

---
+++ src/main/java/com/github/ennoxhd/update/gradle/app/Config.java
@@ -0,0 +1,85 @@
+package com.github.ennoxhd.update.gradle.app;
+
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+class Config {
+
+	private boolean recursive;
+	private Path folder;
+	private String version;
+
+	final boolean isRecursive() {
+		return recursive;
+	}
+
+	private final void setRecursive(final boolean recursive) {
+		this.recursive = recursive;
+	}
+
+	final Path getFolder() {
+		return folder;
+	}
+
+	private final void setFolder(final Path folder) {
+		Objects.requireNonNull(folder);
+		if (!exists(folder))
+			throw new IllegalArgumentException("Not a valid directory path: " + folder);
+		this.folder = folder;
+	}
+
+	final String getVersion() {
+		return version;
+	}
+
+	private final void setVersion(final String version) {
+		Objects.requireNonNull(version);
+		if (!isVersionNumber(version))
+			throw new IllegalArgumentException("Not a x.y[.z] version number: " + version);
+		this.version = version;
+	}
+
+	Config(final boolean recursive, final Path folder, final String version) {
+		setRecursive(recursive);
+		setFolder(folder);
+		setVersion(version);
+	}
+
+	static final boolean isRecursiveArg(final String arg) {
+		return "-r".equals(arg);
+	}
+	
+	static final boolean exists(final Path folder) {
+		Objects.requireNonNull(folder);
+		return folder.toFile().isDirectory();
+	}
+
+	static final boolean isVersionNumber(final String version) {
+		if (version == null || version.isBlank())
+			return false;
+		final Pattern versionString = Pattern.compile("^[0-9]+\\.[0-9]+(\\.[0-9]+)?$");
+		return versionString.matcher(version).find();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+		if (obj == null || !(obj instanceof Config))
+			return false;
+		Config other = (Config) obj;
+		return recursive == other.recursive && Objects.equals(folder, other.folder)
+				&& Objects.equals(version, other.version);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(recursive, folder, version);
+	}
+
+	@Override
+	public String toString() {
+		return "Config[recursive:" + isRecursive() + ",folder:" + getFolder() + ",version:" + getVersion() + "]";
+	}
+}
--- src/main/java/com/github/ennoxhd/update/gradle/app/UpdateGradle.java
+++ src/main/java/com/github/ennoxhd/update/gradle/app/UpdateGradle.java
@@ -1,30 +1,50 @@
 package com.github.ennoxhd.update.gradle.app;
 
-import java.util.Objects;
-import java.util.regex.Pattern;
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class UpdateGradle {
 
 	public static void main(String[] args) {
-		if (args == null || args.length != 1)
-			runtimeException(RuntimeExceptions.NO_VERSION_IN_ARGS);
-		final String version = args[0];
-		if (version == null || !isVersionNumber(version))
-			runtimeException(RuntimeExceptions.NO_VERSION_IN_ARGS);
-
-		System.out.println("Working on update to version " + version + "!");
-		System.out.println("Searching for Gradle projects...");
+		Optional<Config> configOpt = Optional.empty();
+		try {
+			configOpt = analyseArgs(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+			help();
+		}
+		if(configOpt.isEmpty())
+			help();
+		System.out.println(configOpt.get());
+		
+		// TODO: implement
+		
+		//Notes:
+		//for all projects do
+		//.\gradlew.bat wrapper --gradle-version=6.6.1
+		//.\gradlew.bat tasks
+		
+		System.exit(0);
 	}
 
-	private static void runtimeException(RuntimeExceptions message) {
-		Objects.requireNonNull(message);
-		throw new RuntimeException(message.toString());
+	private static void help() {
+		System.out.println("Help for UpdateGradle:\n" + "----------------------\n"
+				+ "Summary: gradlew run --args=\"[-r] <path> <version>\"\n"
+				+ "Example: gradlew run --args=\"-r .. 6.6\"\n\n"
+				+ "     Option      -r: recursively check and update all child folders\n"
+				+ "Requirement    path: path of the folder where gradle project is located or\n"
+				+ "                     with option '-r' where gradle projects are located\n"
+				+ "Requirement version: the new gradle version to update the project(s) to");
+		System.exit(1);
 	}
 
-	private static boolean isVersionNumber(String version) {
-		if (version == null || version.isBlank())
-			return false;
-		Pattern versionString = Pattern.compile("^[0-9]+\\.[0-9]+(\\.[0-9]+)?$");
-		return versionString.matcher(version).find();
+	private static Optional<Config> analyseArgs(final String[] args) {
+		if (args == null || args.length != 2 && args.length != 3)
+			return Optional.empty();
+		final boolean isRecursive = Config.isRecursiveArg(args[0]);
+		final int folderArgIdx = isRecursive ? 1 : 0;
+		final int versionArgIdx = folderArgIdx + 1;		
+		return Optional.ofNullable(
+				new Config(isRecursive, Path.of(args[folderArgIdx]), args[versionArgIdx]));
 	}
 }
